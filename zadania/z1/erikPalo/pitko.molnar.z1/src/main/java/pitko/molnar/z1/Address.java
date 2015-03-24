225432ebd91761668cd04b7fb130803bed12b774
javadneperska <javadneperska@gmail.com>
2015-03-24 16:58:28 +0100

    .

---
+++ zadania/z1/erikPalo/pitko.molnar.z1/src/main/java/pitko/molnar/z1/Address.java
@@ -0,0 +1,44 @@
+package pitko.molnar.z1;
+
+public class Address {
+
+	private String street;
+	private int houseNum;
+	private String city;
+	private String district;
+	private String country;
+	
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public int getHouseNum() {
+		return houseNum;
+	}
+	public void setHouseNum(int houseNum) {
+		this.houseNum = houseNum;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+
+}
