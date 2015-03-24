aa5e63932c4036e7c70653a0535fddb33a7322d2
javadneperska <javadneperska@gmail.com>
2015-03-24 16:41:43 +0100

    add

---
+++ zadania/z1/erikPalo/pitko.molnar.z1/src/main/java/pitko/molnar/z1/Employee.java
@@ -0,0 +1,19 @@
+package pitko.molnar.z1;
+
+public class Employee {
+	private String name;
+	private String surname;
+	private String birthNum;
+	private boolean gender;
+	private String street;
+	private int houseNum;
+	private String city;
+	private String district;
+	private String country;
+	private String expertise;
+	private int lastJobs;
+	private int expYears;
+	private String teachingSubjects;
+	private int age;
+
+}
