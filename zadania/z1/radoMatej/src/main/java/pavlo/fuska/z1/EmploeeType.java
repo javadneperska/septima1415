7984b518f3626c9d6a8039cbf43ff1e264854cad
javadneperska <javadneperska@gmail.com>
2015-03-24 22:17:55 +0100

    Create parsing logic

--- zadania/z1/radoMatej/src/main/java/pavlo/fuska/z1/EmploeeType.java
+++ zadania/z1/radoMatej/src/main/java/pavlo/fuska/z1/EmploeeType.java
@@ -1,5 +1,12 @@
 package pavlo.fuska.z1;
 
 public enum EmploeeType {
-	UCITEL, EXTERNY, PROFESOR, DOCENT, PEDAGOG
+	UCITEL, EXTERNY, PROFESOR, DOCENT, PEDAGOG;
+
+	public static EmploeeType GetType(String type) {
+		for(EmploeeType t : EmploeeType.values())
+			if(type.equalsIgnoreCase(t.toString()))
+				return t;
+		return null;
+	}
 }
