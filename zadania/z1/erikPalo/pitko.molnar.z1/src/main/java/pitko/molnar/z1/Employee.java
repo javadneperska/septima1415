9760a93c845e2dd74287d55f5769fafdf09f9b08
javadneperska <javadneperska@gmail.com>
2015-03-24 17:14:56 +0100

    .

--- zadania/z1/erikPalo/pitko.molnar.z1/src/main/java/pitko/molnar/z1/Employee.java
+++ zadania/z1/erikPalo/pitko.molnar.z1/src/main/java/pitko/molnar/z1/Employee.java
@@ -10,5 +10,22 @@ public class Employee {
 	private int expYears;
 	private String teachingSubjects;
 	private int age;
+	
+	
+	public Employee(String name, String surname, String birthNum,
+			boolean gender, String expertise, int lastJobs, int expYears,
+			String teachingSubjects, int age) {
+		super();
+		this.name = name;
+		this.surname = surname;
+		this.birthNum = birthNum;
+		this.gender = gender;
+		this.expertise = expertise;
+		this.lastJobs = lastJobs;
+		this.expYears = expYears;
+		this.teachingSubjects = teachingSubjects;
+		this.age = age;
+	}
 
+	
 }
