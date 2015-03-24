53436282ed40577dd1a29d4f84fb9083836620a1
javadneperska <javadneperska@gmail.com>
2015-03-24 16:30:38 +0100

    Ok

--- zadania/z1/erikPalo/molnar.pitko.d15/src/main/java/molnar/pitko/d15/Employee.java
+++ zadania/z1/erikPalo/molnar.pitko.d15/src/main/java/molnar/pitko/d15/Employee.java
@@ -1,5 +1,22 @@
 package molnar.pitko.d15;
 
 public class Employee {
+	private String name;
+
+	private String surname;
+	private String birthNum;
+	private boolean gender;
+	private String street;
+	private int houseNum;
+	private String city;
+	private String district;
+	private String country;
+	private String expertise;
+	private int lastJobs;
+	private int expYears;
+	private String teachingSubjects;
+	private int age;
+
+
 
 }
