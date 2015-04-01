6aff0fe29c91c7c11e485b24e0fc53a71fb670d4
javadneperska <javadneperska@gmail.com>
2015-04-01 08:03:08 +0200

    update

--- pisomnePrace/p1/Cars/src/Main.java
+++ pisomnePrace/p1/Cars/src/Main.java
@@ -1,12 +1,25 @@
 
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
+	public static ArrayList<Trans> ARR = new ArrayList<Trans>();
+	public static int CARN;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+	}
+	public static void printCars() {
+		CARN = 1;
+		for (int n = 0; n < ARR.size(); n++) {
+			System.out.format("%2d. " + ARR.get(n).toString(), CARN);
+			CARN++;
+		}
+	}
+	public static void addCar() {
+		ARR.add(new Trans());
 	}
 
 }
