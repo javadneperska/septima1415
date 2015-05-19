72b3b5042659e07022972cab576d62ab35b1664b
javadneperska <javadneperska@gmail.com>
2015-05-19 23:49:52 +0200

    Initial project commit

---
+++ domaceUlohy/d17/fuska.radovan.d17/src/main/java/fuska/radovan/d17/IntFloatLong.java
@@ -0,0 +1,13 @@
+package fuska.radovan.d17;
+
+public class IntFloatLong {
+	public int I;
+	public float F;
+	public long L;
+
+	public IntFloatLong(int i, float f, long l) {
+		I = i;
+		F = f;
+		L = l;
+	}
+}
